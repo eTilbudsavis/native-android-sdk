f68158a20b26b45383721c333bf9858a59b29921
Danny Hvam <oizo@oizo-n55sf-mint.(none)>
2013-06-24 10:28:21 +0200

    added JavaDoc to all methods

--- etasdk/src/com/eTilbudsavis/etasdk/ShoppinglistManager.java
+++ etasdk/src/com/eTilbudsavis/etasdk/ShoppinglistManager.java
@@ -54,7 +54,13 @@ public class ShoppinglistManager {
 		mDbHelper = new DbHelper(mEta.getContext());
 		mCurrentSlId = mEta.getPrefs().getString(PREFS_SHOPPINGLISTMANAGER_USER, null);
 	}
-	
+
+	/**
+	 * Get the shopping list that is currently in use, by the user.<br>
+	 * The current list is synchronized more often, than other lists, since the list is likely
+	 * to be visible to the user. This is useful when juggling multiple lists.
+	 * @return The shopping list currently in use.
+	 */
 	public Shoppinglist getCurrentList() {
 		Shoppinglist sl = null;
 		if (mCurrentSlId == null) {
@@ -70,12 +76,24 @@ public class ShoppinglistManager {
 		return sl;
 	}
 	
+	/**
+	 * Set the shopping list that is currently in use, by the user.<br>
+	 * The current list is synchronized more often, than other lists, since the list is likely
+	 * to be visible to the user. This is useful when juggling multiple lists.
+	 * @param sl - shopping list to make the currently visible list
+	 * @return This ShoppinglistManager
+	 */
 	public ShoppinglistManager setCurrentList(Shoppinglist sl) {
 		mCurrentSlId = sl.getId();
 		mEta.getPrefs().edit().putString(PREFS_SHOPPINGLISTMANAGER_USER, mCurrentSlId).commit();
 		return this;
 	}
 	
+	/**
+	 * Get a shoppinglist from it's ID.
+	 * @param id of the shoppinglist to get
+	 * @return A shopping list, or <code>null</code> if no shopping list exists
+	 */
 	public Shoppinglist getList(String id) {
 		Cursor c = dbGetList(id);
 		return c.moveToFirst() == true ? DbHelper.curToSl(c) : null;
@@ -98,20 +116,26 @@ public class ShoppinglistManager {
 		return list;
 	}
 
-	public void addList(Shoppinglist list) {
-		
-		if (!mustSync()) return;
+	/**
+	 * Add a new shopping list.<br>
+	 * shopping list added to the database, and changes is synchronized to the server if possible.<br>
+	 * 
+	 * @param sl - the new shoppinglist to add to the database, and server
+	 */
+	public void addList(Shoppinglist sl) {
 		
 		// Make sure the UUID does not exist, before adding it
-		while (dbExistsList(list.getId())) {
-			list.setId(Utilities.createUUID());
+		while (dbExistsList(sl.getId())) {
+			sl.setId(Utilities.createUUID());
 		};
 		
 		// Insert the new list to DB
-		dbInsertList(list);
+		dbInsertList(sl);
+
+		if (!mustSync()) return;
 		
 		// Sync online if possible
-		mEta.api().put(Endpoint.getListFromId(user().getId(), list.getId()), new Api.CallbackString() {
+		mEta.api().put(Endpoint.getListFromId(user().getId(), sl.getId()), new Api.CallbackString() {
 			
 			public void onComplete(int statusCode, String data, EtaError error) {
 				Utilities.logd(TAG, "addList", statusCode, data, error);
@@ -124,13 +148,19 @@ public class ShoppinglistManager {
 				notifyListUpdate(null, null, null);
 				
 			}
-		}, list.getApiParams()).execute();
+		}, sl.getApiParams()).execute();
 		
 	}
 
-	public boolean editList(final Shoppinglist list) {
+	/**
+	 * Edit a shopping list already in the database.<br>
+	 * shopping list is replaced in the database, and changes is synchronized to the server if possible.<br>
+	 * @param sl - Shopping list to be replaced
+	 * @return the row ID of the newly inserted row, or -1 if an error occurred
+	 */
+	public boolean editList(final Shoppinglist sl) {
 		
-		long l = dbEditList(list);
+		long l = dbEditList(sl);
 		
 		if (mustSync()) {
 			
@@ -147,7 +177,7 @@ public class ShoppinglistManager {
 				}
 			};
 			
-			mEta.api().put(Endpoint.getListFromId(user().getId(), list.getId()), editItem, list.getApiParams()).execute();
+			mEta.api().put(Endpoint.getListFromId(user().getId(), sl.getId()), editItem, sl.getApiParams()).execute();
 		}
 		
 		return l == -1 ? false : true;
@@ -159,9 +189,18 @@ public class ShoppinglistManager {
 		return sl == null ? 0 : deleteList(sl);
 	}
 
+	/**
+	 * Delete a shopping list.<br>
+	 * shopping list is deleted from the database, and changes is synchronized to the server if possible.<br>
+	 * All shopping list items associated with the shopping list are also deleted.
+	 * @param sl - Shopping list to delete
+	 * @return the number of rows affected.
+	 */
 	public int deleteList(Shoppinglist sl) {
 		
 		int count = dbDeleteList(sl.getId());
+		
+		dbDeleteItems(sl.getId());
 		
 		if (mustSync()) {
 			mEta.api().delete(Endpoint.getListFromId(user().getId(), sl.getId()), new Api.CallbackString() {
@@ -180,6 +219,11 @@ public class ShoppinglistManager {
 		return count;
 	}
 
+	/**
+	 * Get a shopping list item by it's ID
+	 * @param id of the shopping list item
+	 * @return A shopping list item, or <code>null</code> if no item can be found.
+	 */
 	public ShoppinglistItem getItem(String id) {
 		Cursor c = dbGetItem(id);
 		boolean b = c.moveToFirst();
@@ -187,6 +231,14 @@ public class ShoppinglistManager {
 		return b ? DbHelper.curToSli(c) : null;
 	}
 	
+	/**
+	 * Add an item to a shopping list.<br>
+	 * shopping list items is inserted into the database, and changes is synchronized to the server if possible.
+	 * If the shopping list does not exist in the database or the server, a new one is created and synchronized if possible
+	 * TODO: Check if shopping list exists, and do stuff accordingly
+	 * @param sl - shopping list where item should be added to
+	 * @param sli - shopping list item that should be added.
+	 */
 	public void addItem(final Shoppinglist sl, ShoppinglistItem sli) {
 		
 		dbAddItem(sli.setShoppinglistId(sl.getId()));
@@ -210,6 +262,11 @@ public class ShoppinglistManager {
 		}
 	}
 
+	/**
+	 * Insert an updated shopping list item into the db.<br>
+	 * shopping list items is replaced in the database, and changes is synchronized to the server if possible.
+	 * @param sli - shopping list item to edit
+	 */
 	public void editItem(final ShoppinglistItem sli) {
 		
 		dbEditItem(sli);
@@ -234,24 +291,45 @@ public class ShoppinglistManager {
 		}
 	}
 
+	/**
+	 * Delete all items from a shoppinglist where <code>isTicked() == true.</code><br>
+	 * shopping list items is removed from database, and changes is synchronized to the server if possible.
+	 * @param sl - shoppinglist to delete items from
+	 */
 	public void deleteAllTickedItems(Shoppinglist sl) {
 		Bundle b = new Bundle();
 		b.putString(Params.FILTER_DELETE, Shoppinglist.EMPTY_TICKED);
 		deleteAllItems(sl, b);
 	}
 
+	/**
+	 * Delete all items from a shoppinglist where <code>isTicked() == false.</code><br>
+	 * shopping list items is removed from database, and changes is synchronized to the server if possible.
+	 * @param sl - shoppinglist to delete items from
+	 */
 	public void deleteAllUntickedItems(Shoppinglist sl) {
 		Bundle b = new Bundle();
 		b.putString(Params.FILTER_DELETE, Shoppinglist.EMPTY_UNTICKED);
 		deleteAllItems(sl, b);
 	}
 
+	/**
+	 * Delete ALL items from a given shoppinglist.<br>
+	 * shopping list items is removed from database, and changes is synchronized to the server if possible.
+	 * @param sl - shoppinglist to delete items from
+	 */
 	public void deleteAllItems(Shoppinglist sl) {
 		Bundle b = new Bundle();
 		b.putString(Params.FILTER_DELETE, Shoppinglist.EMPTY_ALL);
 		deleteAllItems(sl, b);
 	}
 	
+	/**
+	 * Generic method to delete all items that matches any clauses given in the <code>apiParams</code><br>
+	 * shopping list items is removed from database, and changes is synchronized to the server if possible.
+	 * @param sl to remove items from
+	 * @param apiParams that describe what needs to be deleted
+	 */
 	private void deleteAllItems(Shoppinglist sl, Bundle apiParams) {
 		
 		dbDeleteItems(sl.getId());
@@ -274,10 +352,23 @@ public class ShoppinglistManager {
 		}
 	}
 	
-	public void deleteItem(ShoppinglistItem sli) {
-		
+	/**
+	 * TODO: finish this method<br>
+	 * Deletes a given shopping list item<br>
+	 * shopping list items is removed from database, and changes is synchronized to the server if possible.
+	 * @param sli to delete from the db
+	 * @return the number of rows affected
+	 */
+	public int deleteItem(ShoppinglistItem sli) {
+		return dbDeleteItem(sli.getId());
 	}
 
+	/**
+	 * Sync all shopping lists.<br>
+	 * This is run at certain intervals if startSync() has been called.<br>
+	 * startSync() is called if Eta.onResume() is called.
+	 * @param shoppinglist update
+	 */
 	public void syncLists() {
 		
 		if (!mustSync()) return;
@@ -290,7 +381,7 @@ public class ShoppinglistManager {
 				
 				if (Utilities.isSuccess(statusCode)) {
 					ArrayList<Shoppinglist> sl = Shoppinglist.fromJSONArray(data);
-					mergeLists(sl);
+					mergeLists(sl, getAllLists());
 				} else {
 					mEta.addError(error);
 					Utilities.logd(TAG, error.toString());
@@ -301,15 +392,21 @@ public class ShoppinglistManager {
 		mEta.api().get(Endpoint.getListList(user().getId()), listener).execute();
 	}
 	
-	private void mergeLists(ArrayList<Shoppinglist> lists) {
+	/**
+	 * Method, that will merge to sets of shopping lists and find which lists
+	 * have been edited, added and deleted. Finally it will <code>notifyListUpdate()</code>
+	 * @param newLists - The newly recieved items
+	 * @param oldLists - The old list of items
+	 */
+	private void mergeLists(ArrayList<Shoppinglist> newLists, ArrayList<Shoppinglist> oldLists) {
 		
 		HashMap<String, Shoppinglist> dblist = new HashMap<String, Shoppinglist>();
-		for (Shoppinglist sl : getAllLists()) {
+		for (Shoppinglist sl : oldLists) {
 			dblist.put(sl.getId(), sl);
 		}
 
 		HashMap<String, Shoppinglist> tmplists = new HashMap<String, Shoppinglist>();
-		for (Shoppinglist sl : lists) {
+		for (Shoppinglist sl : newLists) {
 			tmplists.put(sl.getId(), sl);
 		}
 		
@@ -345,7 +442,13 @@ public class ShoppinglistManager {
 		}
 		notifyListUpdate(added, deleted, edited);
 	}
-	
+
+	/**
+	 * Sync all shopping list items, in all shopping lists.<br>
+	 * This is run at certain intervals if startSync() has been called.<br>
+	 * startSync() is called if Eta.onResume() is called.
+	 * @param shoppinglist update
+	 */
 	public void syncItems() {
 
 		if (!mustSync()) return;
@@ -386,7 +489,15 @@ public class ShoppinglistManager {
 		
 	}
 	
-	private void syncItems(final Shoppinglist sl) {
+	/**
+	 * Sync all shopping list items, associated with the given shopping list.<br>
+	 * This is run at certain intervals if startSync() has been called.<br>
+	 * startSync() is called if Eta.onResume() is called.
+	 * @param shoppinglist update
+	 */
+	public void syncItems(Shoppinglist shoppinglist) {
+		
+		final Shoppinglist sl = shoppinglist;
 		
 		sl.setSyncing(true);
 		
@@ -416,6 +527,7 @@ public class ShoppinglistManager {
 	
 	/**
 	 * Start synchronization with server
+	 * If Eta.onResume() is called, there should be no need to call this.
 	 */
 	public void startSync() {
 		if (mustSync()) {
@@ -426,16 +538,26 @@ public class ShoppinglistManager {
 	
 	/**
 	 * Stop synchronization to server
+	 * If Eta.onPause() is called, there should be no need to call this.
 	 */
 	public void stopSync() {
 		mEta.getHandler().removeCallbacks(mListSync);
 		mEta.getHandler().removeCallbacks(mItemSync);
 	}
 
+	/**
+	 * Returns User<br>
+	 * Just a convenience wrapper
+	 * @return
+	 */
 	private User user() {
 		return mEta.getSession().getUser();
 	}
 
+	/**
+	 * Checks if a user is logged in, and are able to sync items.
+	 * @return true if we can sync, else false.
+	 */
 	private boolean mustSync() {
 
 		if (!user().isLoggedIn()) {
@@ -447,7 +569,8 @@ public class ShoppinglistManager {
 	}
 	
 	/**
-	 * Open a connection to database
+	 * Open a connection to database<br>
+	 * If Eta.onResume() is called, there should be no need to call this.
 	 */
 	public void openDB() {
 		mDatabase = mDbHelper.getWritableDatabase();
@@ -460,24 +583,46 @@ public class ShoppinglistManager {
 	}
 	
 	/**
-	 * Close the database connection
+	 * Close the database connection<br>
+	 * If Eta.onPause() is called, there should be no need to call this.
 	 */
 	public void closeDB() {
 		mDbHelper.close();
 	}
 
+	/**
+	 * Deletes all tables (<i>shoppinglists</i> and <i>shoppinglistitems</i>)
+	 * and creates two new tables<br>
+	 * This cannot be undone.
+	 */
 	public void clearDatabase() {
 		mDbHelper.onUpgrade(mDatabase, 0, 0);
 	}
 	
+	/**
+	 * Insert new shoppinglist into db
+	 * @param list to insert
+	 * @return the row ID of the newly inserted row, or -1 if an error occurred
+	 */
 	private long dbInsertList(Shoppinglist list) {
 		return mDatabase.insert(SHOPPINGLIST, null, DbHelper.slToCV(list));
 	}
 
+	/**
+	 * Get a shopping list by it's id
+	 * @param id to get
+	 * @return A cursor with a shopping list that matches the id, or empty cursor 
+	 */
 	private Cursor dbGetList(String id) {
 		return mDatabase.query(SHOPPINGLIST, null, DbHelper.ID + "=?", new String[]{id}, null, null, null, null);
 	}
 
+	/**
+	 * Check if a list by a given id exists in db.<br>
+	 * Good for checking if a UUID exists before inserting a new list.
+	 * @param id to check for
+	 * @return true if it exists, else false
+	 */
 	private boolean dbExistsList(String id) {
 		Cursor c = dbGetList(id);
 		boolean res = c.moveToFirst();
@@ -485,28 +630,53 @@ public class ShoppinglistManager {
 		return res;
 	}
 	
+	/**
+	 * Get a shopping list from it's readable name
+	 * @param name of the shopping list
+	 * @return Cursor with shopping lists that matches name
+	 */
 	private Cursor dbGetListFromName(String name) {
 		return mDatabase.query(SHOPPINGLIST, null, DbHelper.NAME + "=?", new String[]{name}, null, null, null);
 	}
+	
+	/**
+	 * Get all shoppinglists
+	 * @return Cursor with shoppinglists from db
+	 */
 	private Cursor dbGetAllLists() {
 		return mDatabase.query(SHOPPINGLIST, null, null, null, null, null, null);
 	}
 	
+	/**
+	 * Delete a list, from the db
+	 * @param shoppinglistId to delete
+	 * @return the number of rows affected.
+	 */
 	private int dbDeleteList(String shoppinglistId) {
 		return mDatabase.delete(SHOPPINGLIST, DbHelper.ID + "=?", new String[]{shoppinglistId});
 	}
 	
+	/**
+	 * Replaces a shoppinglist, that have been updated in some way
+	 * @param list that have been edited
+	 * @return the row ID of the newly inserted row, or -1 if an error occurred
+	 */
 	private long dbEditList(Shoppinglist list) {
 		return mDatabase.replace(SHOPPINGLIST, null, DbHelper.slToCV(list));
 	}
 
-	
+	/**
+	 * Adds a list of items to db, IF they do not yet exist, else nothing
+	 * @param items to insert
+	 * @return true if all items have successfully been inserted, else false
+	 */
 	private boolean dbAddItems(ArrayList<ShoppinglistItem> items) {
+		boolean resp = true;
 		for (ShoppinglistItem sli : items)
 			if (dbAddItem(sli) == -1)
-				return false;
+				resp = false;
 		
-		return true;
+		return resp;
 	}
 	
 	/**
