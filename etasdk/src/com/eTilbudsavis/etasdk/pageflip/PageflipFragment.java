fe3a1e89554b980889e0642ff0e1f1e699a627e9
Danny Hvam <dkhvam@gmail.com>
2014-11-21 13:42:05 +0100

    Added JavaDoc

--- etasdk/src/com/eTilbudsavis/etasdk/pageflip/PageflipFragment.java
+++ etasdk/src/com/eTilbudsavis/etasdk/pageflip/PageflipFragment.java
@@ -73,10 +73,21 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 	int mOutOfBoundsCount = 0;
 	boolean mOutOfBoundsCalled = false;
 	
+	/**
+	 * Creates a new instance of {@link PageflipFragment}, to replace or insert into a current layout.
+	 * @param c The catalog to show
+	 * @return A Fragment
+	 */
 	public static PageflipFragment newInstance(Catalog c) {
 		return newInstance(c, 1);
 	}
 
+	/**
+	 * Creates a new instance of {@link PageflipFragment}, to replace or insert into a current layout.
+	 * @param c The catalog to show
+	 * @param page the page number to start at
+	 * @return A Fragment
+	 */
 	public static PageflipFragment newInstance(Catalog c, int page) {
 		Bundle b = new Bundle();
 		b.putSerializable(ARG_CATALOG, c);
@@ -86,6 +97,12 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		return f;
 	}
 
+	/**
+	 * Creates a new instance of {@link PageflipFragment}, to replace or insert into a current layout.
+	 * @param catalogId The is of the catalog to show
+	 * @param page the page number to start at
+	 * @return A Fragment
+	 */
 	public static PageflipFragment newInstance(String catalogId, int page) {
 		Bundle b = new Bundle();
 		b.putString(ARG_CATALOG_ID, catalogId);
@@ -140,6 +157,10 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		
 	}
 	
+	/**
+	 * Called to setup the view, on create and resume events.
+	 * @param removeParent Whether to remove the View from the parent view (on e.g. configuration changes)
+	 */
 	private void setUpView(boolean removeParent) {
 		
 		if (mFrame == null) {
@@ -297,22 +318,43 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		}
 	}
 	
+	/**
+	 * Get the {@link PageflipListener}.
+	 * @return The listener, or <code>null</code>.
+	 */
 	public PageflipListener getListener() {
 		return mWrapperListener.getListener();
 	}
 	
+	/**
+	 * Get the {@link PageflipFragment} wrapper listener, primarily used for debugging.
+	 * @return The wrapper listener
+	 */
 	public PageflipListener getWrapperListener() {
 		return mWrapperListener;
 	}
 	
+	/**
+	 * Set a listener to call on {@link PageflipFragment} events.
+	 * @param l The listener
+	 */
 	public void setPageflipListener(PageflipListener l) {
 		mWrapperListener.setListener(l);;
 	}
 	
+	/**
+	 * Get the pages currently being displayed in the {@link PageflipFragment}.
+	 * @return An array of pages being displayed
+	 */
 	public int[] getPages() {
 		return PageflipUtils.positionToPages(mCurrentPosition, mCatalog.getPageCount(), mLandscape);
 	}
 	
+	/**
+	 * Set the {@link PageflipFragment} to show the given page number in the catalog.
+	 * Note that page number doesn't directly correlate to the position of the {@link PageflipViewPager}.
+	 * @param page The page to turn to
+	 */
 	public void setPage(int page) {
 		if (PageflipUtils.isValidPage(mCatalog, page)) {
 			setPosition(PageflipUtils.pageToPosition(page, mLandscape));
@@ -321,29 +363,55 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		}
 	}
 	
+	/**
+	 * Get the current position of the {@link PageflipViewPager}.
+	 * @return The current position
+	 */
 	public int getPosition() {
 		return mCurrentPosition;
 	}
 	
+	/**
+	 * Set the position of the {@link PageflipViewPager}. 
+	 * Note that this does not correlate directly to the catalog page number.
+	 * @param position A position
+	 */
 	public void setPosition(int position) {
 		mCurrentPosition = position;
 		if (mPager != null) {
 			mPager.setCurrentItem(mCurrentPosition);
 		}
 	}
-
+	
+	/**
+	 * Go to the next page in the catalog
+	 */
 	public void nextPage() {
 		mPager.setCurrentItem(mCurrentPosition+1, true);
 	}
-
+	
+	/**
+	 * Go to the previous page in the catalog
+	 */
 	public void previousPage() {
 		mPager.setCurrentItem(mCurrentPosition-1, true);
 	}
 	
+	/**
+	 * Method for determining if the {@link PageflipFragment} is ready.
+	 * It checks if the {@link PageflipViewPager} has an {@link PageflipAdapter} attached.
+	 * @return true if the fragment if ready, else false.
+	 */
 	public boolean isReady() {
 		return mAdapter != null;
 	}
-	
+
+	/**
+	 * Set the {@link Catalog} that you want to display.
+	 * This is unnecessary if you have created the fragment with one of the provided 
+	 * {@link PageflipFragment} newInstance methods.
+	 * @param c A catalog to display
+	 */
 	public void setCatalog(Catalog c) {
 		if (c != null) {
 			mCatalog = c;
@@ -351,10 +419,20 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		}
 	}
 	
+	/**
+	 * Set the id of the {@link Catalog#getId() catalog} that you want to display.
+	 * This is unnecessary if you have created the fragment with one of the provided 
+	 * {@link PageflipFragment} newInstance methods.
+	 * @param catalogId A catalog id
+	 */
 	public void setCatalogId(String catalogId) {
 		mCatalogId = catalogId;
 	}
 	
+	/**
+	 * Method for instantiating the {@link PageflipFragment}. 
+	 * This will perform all needed actions in order to get the show started. 
+	 */
 	public void start() {
 		
 		if (mPageflipStarted) {
@@ -391,6 +469,11 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		super.onPause();
 	}
 	
+	/**
+	 * Method for letting the fragment know of backpressed events.
+	 * Currently this does nothing.
+	 * @return false
+	 */
 	public boolean onBackPressed() {
 		return false;
 	}
@@ -431,7 +514,7 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 	public Catalog getCatalog() {
 		return mCatalog;
 	}
-
+	
 	public boolean isLandscape() {
 		return mLandscape;
 	}
@@ -448,6 +531,9 @@ public class PageflipFragment extends Fragment implements PageCallback, OnPageCh
 		return mViewSessionUuid;
 	}
 	
+	/**
+	 * A wrapper class for the users {@link PageflipListener}. Used to do some debugging.
+	 */
 	protected class PageflipListenerWrapper implements PageflipListener {
 		
 		protected PageflipListener mListener;
