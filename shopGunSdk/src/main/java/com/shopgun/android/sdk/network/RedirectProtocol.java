6e829eee98459763edb4f89461d7324359afe485
Danny Hvam <dkhvam@gmail.com>
2016-05-18 14:07:37 +0200

    JavaDoc

--- shopGunSdk/src/main/java/com/shopgun/android/sdk/network/RedirectProtocol.java
+++ shopGunSdk/src/main/java/com/shopgun/android/sdk/network/RedirectProtocol.java
@@ -24,18 +24,37 @@ import java.util.ArrayList;
 
 public interface RedirectProtocol {
 
+    /**
+     * Determine if a request should be redirected to a new location, given the {@link HttpResponse} from the server
+     * @param request A {@link Request}
+     * @param response The {@link HttpResponse response} from the server, including statusCode and headers. But no {@link org.apache.http.HttpEntity HttpEntity}.
+     * @param previouslyVisited All previously visited links in this redirect chain
+     * @return {@code true} if the network should redirect, else {@code false}
+     * @throws IOException
+     */
     boolean isRedirectRequested(Request<?> request, HttpResponse response, ArrayList<URL> previouslyVisited) throws IOException;
 
     /**
-     * When the final {@link URL} in a chain of redirects have been resolved, this method is called.
-     * To avoid network overhead of downloading an irrelevant body content, this method can optionally be
-     * {@link Override overridden} and throw an exception.
-     * @param request The request being performed
-     * @param urls The {@link ArrayList list} of resolved {@link URL}.
+     * Determine the new location the request is expected to be redirected to given the {@link HttpResponse} from the server
+     * and the current execution context.
+     * @param request A {@link Request}
+     * @param response The {@link HttpResponse response} from the server, including statusCode and headers. But no {@link org.apache.http.HttpEntity HttpEntity}.
+     * @param previouslyVisited All previously visited links in this redirect chain
+     * @return A redirect {@link URL}
+     * @throws IOException
+     */
+    URL getRedirectLocation(Request<?> request, HttpResponse response, ArrayList<URL> previouslyVisited) throws IOException;
+
+    /**
+     * When the final {@link URL} in a chain of redirects have been resolved, this method is called. In some instances
+     * the etilbudsavis.dk APi will return the content of a request in the redirect url, rather than in the body, this
+     * would be a great time to resolve those.
+     *
+     * <p>To avoid network overhead of downloading an irrelevant body content, this method can optionally throw an exception.</p>
+     * @param request A {@link Request}
+     * @param urls All previously visited links in this redirect chain
      * @throws IOException to avoid reading the body of the response.
      */
-    URL getRedirectLocation(Request<?> request, HttpResponse response, ArrayList<URL> urls) throws IOException;
-
     void onRedirectComplete(Request<?> request, ArrayList<URL> urls) throws IOException;
 
 }
