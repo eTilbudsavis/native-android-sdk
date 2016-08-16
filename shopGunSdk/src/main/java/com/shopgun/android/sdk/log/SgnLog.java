3e8614c768d9d9975dfc8c29e60b36454c0f434a
dkhvam@gmail.com <Danny Hvam>
2016-08-16 09:48:54 +0200

    removed a logging interface

--- shopGunSdk/src/main/java/com/shopgun/android/sdk/log/SgnLog.java
+++ shopGunSdk/src/main/java/com/shopgun/android/sdk/log/SgnLog.java
@@ -1,14 +1,17 @@
 package com.shopgun.android.sdk.log;
 
+import com.shopgun.android.utils.log.LogCatLogger;
 import com.shopgun.android.utils.log.Logger;
-import com.shopgun.android.utils.log.QuietLogger;
 
+/**
+ * Static class for easy access to logging with {@link Logger}
+ */
 public class SgnLog {
 
-    private static volatile Logger mLogger = new QuietLogger();
+    private static volatile Logger mLogger = new LogCatLogger(0);
 
     public static void setLogger(Logger logger) {
-        mLogger = logger == null ? new QuietLogger() : logger;
+        mLogger = (logger == null ? new LogCatLogger(0) : logger);
     }
 
     public static int v(String tag, String msg) {
