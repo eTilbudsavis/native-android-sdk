c9b0a10bad5cda9da2e8166167bde9b0ac640442
dkhvam@gmail.com <Danny Hvam>
2016-11-10 10:47:41 +0100

    removed debugging code

--- shopGunSdk/src/main/java/com/shopgun/android/sdk/pagedpublicationkit/PagedPublicationFragment.java
+++ shopGunSdk/src/main/java/com/shopgun/android/sdk/pagedpublicationkit/PagedPublicationFragment.java
@@ -1,7 +1,6 @@
 package com.shopgun.android.sdk.pagedpublicationkit;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -136,12 +135,7 @@ public class PagedPublicationFragment extends VersoFragment {
                 return;
             }
             showLoaderView();
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mConfig.load(mOnLoadComplete);
-                }
-            }, 2000);
+            mConfig.load(mOnLoadComplete);
         }
     }
 
