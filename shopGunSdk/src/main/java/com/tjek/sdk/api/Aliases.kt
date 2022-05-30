10607791b332a160ab589e42d1f9af9ca0f2e4a7
Agnese Bussone <agb@tjek.com>
2022-05-30 16:53:54 +0200

    Change date parser to account for different offset types

--- shopGunSdk/src/main/java/com/tjek/sdk/api/Aliases.kt
+++ shopGunSdk/src/main/java/com/tjek/sdk/api/Aliases.kt
@@ -1,4 +1,8 @@
 package com.tjek.sdk.api
 
+import java.time.OffsetDateTime
+
 typealias Id = String
 typealias HexColor = Int
+typealias ValidityDateStr = String
+typealias ValidityDate = OffsetDateTime
--- shopGunSdk/src/main/java/com/tjek/sdk/api/DateUtils.kt
+++ shopGunSdk/src/main/java/com/tjek/sdk/api/DateUtils.kt
@@ -1,23 +1,33 @@
 package com.tjek.sdk.api
 
 import com.tjek.sdk.TjekLogCat
-import java.lang.Exception
-import java.time.Instant
-import java.time.ZoneOffset
-import java.time.ZonedDateTime
+import java.time.*
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
 import java.util.*
 
-typealias ValidityDateStr = String
-typealias ValidityDate = ZonedDateTime
+// V2 date format is in the form yyyy-MM-dd'T'HH:mm:ssZZZZ.
+// I want to be able to parse both types of offset that this format can give:
+// 2000-05-06T22:00:00+0000
+// 2000-05-06T22:00:00+00:00
 
-/** The date format as returned from the server  */
-const val API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
-val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern(API_DATE_FORMAT, Locale.US)
+
+private val offset1: DateTimeFormatter = DateTimeFormatterBuilder()
+    .appendOffset("+HHMM", "0000")
+    .toFormatter(Locale.ENGLISH)
+private val offset2: DateTimeFormatter = DateTimeFormatterBuilder()
+    .appendOffset("+HH:MM", "+00:00")
+    .toFormatter(Locale.ENGLISH)
+private var parser: DateTimeFormatter = DateTimeFormatterBuilder()
+    .append(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+    .appendOptional(offset1)
+    .appendOptional(offset2)
+    .parseCaseInsensitive()
+    .toFormatter(Locale.ENGLISH)
 
 fun ValidityDateStr.parse(): ValidityDate? {
     return try {
-        ZonedDateTime.parse(this, DATE_FORMATTER)
+        OffsetDateTime.parse(this, parser)
     }catch (e: Exception) {
         TjekLogCat.e(e.message ?: "date parsing fail for $this")
         null
@@ -25,11 +35,11 @@ fun ValidityDateStr.parse(): ValidityDate? {
 }
 
 fun distantPast(): ValidityDate {
-    return Instant.ofEpochMilli(Long.MIN_VALUE).atZone(ZoneOffset.UTC)
+    return OffsetDateTime.MIN
 }
 
 fun distantFuture(): ValidityDate {
-    return Instant.ofEpochMilli(Long.MAX_VALUE).atZone(ZoneOffset.UTC)
+    return OffsetDateTime.MAX
 }
 
 
