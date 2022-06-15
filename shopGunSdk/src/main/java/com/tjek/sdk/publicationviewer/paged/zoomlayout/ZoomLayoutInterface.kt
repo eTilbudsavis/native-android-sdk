0af733a2e618bf7dc41bffc907367fa3318f91f0
Agnese Bussone <agb@tjek.com>
2022-06-15 17:09:24 +0200

    Move events in a single file for better overview

--- shopGunSdk/src/main/java/com/tjek/sdk/publicationviewer/paged/zoomlayout/ZoomLayoutInterface.kt
+++ shopGunSdk/src/main/java/com/tjek/sdk/publicationviewer/paged/zoomlayout/ZoomLayoutInterface.kt
@@ -2,5 +2,33 @@ package com.tjek.sdk.publicationviewer.paged.zoomlayout
 
 interface ZoomLayoutInterface {
 
+    /**
+     * Receive events from the ZoomLayout.
+     * Each event contains the reference to the view and the relevant information for the event.
+     * The receiver has the possibility to change the view parameters.
+     *
+     * Return: true if the event was consumed, false otherwise
+     */
     fun onZoomLayoutEvent(event: ZoomLayoutEvent): Boolean
+}
+
+sealed class ZoomLayoutEvent {
+
+    // Zoom
+    data class ZoomBegin(val view: ZoomLayout, val scale: Float) : ZoomLayoutEvent()
+    data class Zoom(val view: ZoomLayout, val scale: Float) : ZoomLayoutEvent()
+    data class ZoomEnd(val view: ZoomLayout, val scale: Float) : ZoomLayoutEvent()
+
+    // Pan
+    data class PanBegin(val view: ZoomLayout) : ZoomLayoutEvent()
+    data class Pan(val view: ZoomLayout) : ZoomLayoutEvent()
+    data class PanEnd(val view: ZoomLayout) : ZoomLayoutEvent()
+
+    // Touch
+    data class Touch(val view: ZoomLayout, val action: Int, val info: TapInfo) : ZoomLayoutEvent()
+
+    // Tap
+    data class Tap(val view: ZoomLayout, val info: TapInfo) : ZoomLayoutEvent()
+    data class DoubleTap(val view: ZoomLayout, val info: TapInfo) : ZoomLayoutEvent()
+    data class LongTap(val view: ZoomLayout, val info: TapInfo) : ZoomLayoutEvent()
 }
