62afa0d4d9839777f461ce0df539b39bd0da57e4
Agnese Bussone <agb@tjek.com>
2022-08-08 11:48:41 +0200

    Add default coordinates set to demo app to showcase different countries

--- shopGunSdkDemo/src/main/java/com/shopgun/android/sdk/demo/Constants.kt
+++ shopGunSdkDemo/src/main/java/com/shopgun/android/sdk/demo/Constants.kt
@@ -4,4 +4,6 @@ import com.tjek.sdk.api.models.Coordinate
 
 object Constants {
     val TJEK_HQ = Coordinate(55.67384205604396, 12.59113808366715)
+    val OSLO = Coordinate(59.91471272417154, 10.752341917995402)
+    val STOCKHOLM = Coordinate(59.33334842539517, 18.06938528409325)
 }
--- shopGunSdkDemo/src/main/java/com/shopgun/android/sdk/demo/MainActivity.kt
+++ shopGunSdkDemo/src/main/java/com/shopgun/android/sdk/demo/MainActivity.kt
@@ -5,25 +5,40 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.shopgun.android.sdk.demo.Constants.OSLO
+import com.shopgun.android.sdk.demo.Constants.STOCKHOLM
 import com.shopgun.android.sdk.demo.Constants.TJEK_HQ
 import com.shopgun.android.sdk.demo.publication.PublicationListActivity
 import com.tjek.sdk.api.models.Coordinate
 
 class MainActivity : AppCompatActivity() {
+
+    private var lat: EditText? = null
+    private var lng: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
 
-        val lat = findViewById<EditText>(R.id.lat)
-        lat?.setText(TJEK_HQ.latitude.toString())
-        val lng = findViewById<EditText>(R.id.lng)
-        lng?.setText(TJEK_HQ.longitude.toString())
+        lat = findViewById(R.id.lat)
+        lng = findViewById(R.id.lng)
+        setCoordinates(TJEK_HQ)
 
         findViewById<Button>(R.id.publication_list)?.setOnClickListener {
-            val coordinate = Coordinate(lat.text.toString().toDouble(), lng.text.toString().toDouble())
+            val coordinate = Coordinate(lat?.text.toString().toDouble(), lng?.text.toString().toDouble())
             startActivity(Intent(this, PublicationListActivity::class.java).apply {
                 putExtra("coordinate", coordinate)
             })
         }
+
+        findViewById<Button>(R.id.tjek_hq)?.setOnClickListener { setCoordinates(TJEK_HQ) }
+        findViewById<Button>(R.id.oslo)?.setOnClickListener { setCoordinates(OSLO) }
+        findViewById<Button>(R.id.stockholm)?.setOnClickListener { setCoordinates(STOCKHOLM) }
+    }
+
+    private fun setCoordinates(coordinate: Coordinate){
+        lat?.setText(coordinate.latitude.toString())
+        lng?.setText(coordinate.longitude.toString())
+
     }
 }
